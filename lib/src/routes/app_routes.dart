025c0aadd11feaa1d5fcddfe5d388f394b6f91bb
anhhuy007 <imanhhuy007@gmail.com>
2024-03-28 20:48:19 +0700

    feat(init): create folder structure, add navigator service

---
+++ lib/src/routes/app_routes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class AppRoute {
+  static String splashScreen = '/splash_screen';
+  static String loginScreen = '/login_screen';
+  static String registerScreen = '/register_screen';
+  static String homeScreen = '/home_screen';
+  static String homeScreenContainer = '/home_screen_container';
+
+  static Map<String, WidgetBuilder> routes = {
+    // splashScreen: (context) => SplashScreen(),
+    // loginScreen: (context) => LoginScreen(),
+    // registerScreen: (context) => RegisterScreen(),
+    // homeScreen: (context) => HomeScreen(),
+    // homeScreenContainer: (context) => HomeScreenContainer(),
+  };
+}
